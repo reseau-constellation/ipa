078c2287c11400b40be89af68f71be004ae4df75
julienmalard <julien.malard@mail.mcgill.ca>
2022-03-08 21:49:13 +0700

    Épinglage récursif pour bd profil (image, noms, etc.)

--- src/client.ts
+++ src/client.ts
@@ -241,8 +241,8 @@ export default class ClientConstellation extends EventEmitter {
     this.automatisations = new Automatisations(this, idBdAuto!);
     await this.automatisations.initialiser();
 
+    this.épingles!.épinglerBd(idBdProfil!)  // Celle-ci doit être récursive
     for (const idBd in [
-      idBdProfil,
       idBdBDs,
       idBdVariables,
       idBdRéseau,
