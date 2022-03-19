9df6181be9b755bd3cd983823fc92780017b6097
julienmalard <julien.malard@mail.mcgill.ca>
2022-03-19 13:01:04 +0700

    Ajouter option liste colonnes

--- src/importateur/xlsx.ts
+++ src/importateur/xlsx.ts
@@ -19,19 +19,29 @@ export default class ImportateurFeuilleCalcul {
 
   obtDonnées(
     nomTableau: string,
-    cols: { [key: string]: string }
+    cols: { [key: string]: string } | string[]
   ): { [key: string]: string | number }[] {
     const feuille = this.doc.Sheets[nomTableau];
     const données = utils.sheet_to_json(feuille) as {
       [key: string]: string | number;
     }[];
 
-    return données.map((d) =>
-      Object.fromEntries(
-        Object.keys(d)
-          .filter((c) => Object.keys(cols).includes(c))
-          .map((c) => [cols[c], d[c]])
-      )
-    );
+    if (Array.isArray(cols)) {
+      return données.map((d) =>
+        Object.fromEntries(
+          Object.keys(d)
+            .filter((c) => cols.includes(c))
+            .map((c) => [c, d[c]])
+        )
+      );
+    } else {
+      return données.map((d) =>
+        Object.fromEntries(
+          Object.keys(d)
+            .filter((c) => Object.keys(cols).includes(c))
+            .map((c) => [cols[c], d[c]])
+        )
+      );
+    }
   }
 }
