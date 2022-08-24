fb593a76d2d0ca804e39e9e3147eb58af5dc356f
julienmalard <julien.malard@mail.mcgill.ca>
2022-08-24 17:52:08 +0200

    Changer noms test

--- src/licences.test.ts
+++ src/licences.test.ts
@@ -3,7 +3,7 @@ import isArray from "lodash/isArray";
 import { licences, infoLicences } from "@/licences";
 
 describe("Licences", function () {
-  it("licences", () => {
+  test("licences", () => {
     expect(isArray(licences)).toBe(true);
     expect(licences).toHaveLength(Object.keys(infoLicences).length);
   });
