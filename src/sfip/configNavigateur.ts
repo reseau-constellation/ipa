ab1b88ea0877838889fd74cede79874b75c1f22c
julienmalard <julien.malard@mail.mcgill.ca>
2023-02-06 10:24:11 +0100

    version

--- src/sfip/configNavigateur.ts
+++ src/sfip/configNavigateur.ts
@@ -2,10 +2,11 @@ import { webRTCStar } from "@libp2p/webrtc-star";
 import { webSockets } from "@libp2p/websockets";
 import { webRTCDirect } from "@libp2p/webrtc-direct";
 import { create } from "ipfs";
-
+const webRTC = webRTCStar()
 const config: Parameters<typeof create>[0] = {
   libp2p: {
-    transports: [webRTCStar(), webSockets(), webRTCDirect()],
+    transports: [webRTC.transport, webSockets(), webRTCDirect()],
+    peerDiscovery: [webRTC.discovery]
   }
 };
 export default config
