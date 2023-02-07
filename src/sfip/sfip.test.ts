fef26308057b521d1ea94cf2fc8479cd213ce648
julienmalard <julien.malard@mail.mcgill.ca>
2023-02-07 17:14:26 +0100

    Premier test aegir

--- src/sfip/sfip.test.ts
+++ src/sfip/sfip.test.ts
@@ -3,6 +3,6 @@ import initSFIP from "@/sfip/index.js";
 describe("SFIP", function () {
     test("Initialiser", async () => {
         const sfip = await initSFIP();
-        console.log(sfip)
+        console.log((await sfip.id()).id.toCID().toString())
     }, 10000)
 })
