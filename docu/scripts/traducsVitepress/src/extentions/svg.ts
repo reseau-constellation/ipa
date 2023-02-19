68fa861a4b905fcce094346deda1773a4f5b7480
julienmalard <julien.malard@mail.mcgill.ca>
2023-02-19 23:01:19 +0100

    xml2js

--- docu/scripts/traducsVitepress/src/extentions/svg.ts
+++ docu/scripts/traducsVitepress/src/extentions/svg.ts
@@ -1,4 +1,5 @@
 import { JSDOM } from "jsdom";
+import xml2js from "xml2js";
 
 import { Extention } from "./extention.js";
 
@@ -10,19 +11,20 @@ export class ExtentionSvg extends Extention {
   }: {
     texte: string;
   }): Promise<{ [clef: string]: string }> {
-    const lexé = await xml2js.Parser().parseStringPromise(svg);
-    return lexé.svg.text.map((t) => t._);
+    const lexé = await new xml2js.Parser().parseStringPromise(texte);
+    return lexé.svg.text.map((t: {_: string}) => t._);
   };
 
   async compiler({ texte, traducs, fichier }: { texte: string; traducs: { [clef: string]: string; }; fichier: string; }): Promise<string> {
-    const lexé = await xml2js.Parser().parseStringPromise(texte);
-          for (const t of lexé.svg.text) {
-            const clef = calculerClef(fichier, t._);
-            t._ = traductions[clef] || t._;
-          }
-          const reconstitué = new xml2js.Builder().buildObject(lexé);
-          texteFinal.push(reconstitué);
-          break;
+    const lexé = await new xml2js.Parser().parseStringPromise(texte);
+    const texteFinal: string[] = []
+    for (const t of lexé.svg.text) {
+      const clef = fichier + "." + t._;
+      t._ = traducs[clef] || t._;
+    }
+    const reconstitué = new xml2js.Builder().buildObject(lexé);
+    texteFinal.push(reconstitué);
+    return texteFinal.join("\n");
   }
   
 }
