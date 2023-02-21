cce577d538fb0d2f21d2c7d1ab78933169ea36cb
julienmalard <julien.malard@mail.mcgill.ca>
2023-02-21 14:17:20 +0100

    Passé à aegir

--- src/sfip/sfip.test.ts
+++
@@ -1,8 +0,0 @@
-import initSFIP from "@/sfip/index.js";
-
-describe("SFIP", function () {
-  test("Initialiser", async () => {
-    const sfip = await initSFIP();
-    console.log((await sfip.id()).id.toCID().toString());
-  }, 10000);
-});
