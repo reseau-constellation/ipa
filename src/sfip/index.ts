2fb0f1cf2eb8f84bd364c53f678a127e6bfd9034
julienmalard <julien.malard@mail.mcgill.ca>
2023-02-28 12:14:49 +0100

    Éviter une erreur de connection webtrc-star lorsqu'on est hors ligne

--- src/sfip/index.ts
+++ src/sfip/index.ts
@@ -11,6 +11,7 @@ import { noise } from "@chainsafe/libp2p-noise";
 import mergeOptions from "merge-options";
 import type { IPFS } from "ipfs-core";
 import type { Options } from "ipfs-core";
+import { FaultTolerance } from "@libp2p/interface-transport"
 
 const obtConfigPlateforme = async (): Promise<Parameters<typeof create>[0]> => {
   let configPlateforme: Parameters<typeof create>[0];
@@ -38,6 +39,9 @@ const obtConfigCommun = (): Options => {
     libp2p: {
       streamMuxers: [mplex()],
       connectionEncryption: [noise()],
+      transportManager: {
+        faultTolerance: FaultTolerance.NO_FATAL
+      }
     },
     relay: { enabled: true, hop: { enabled: true, active: true } },
   };
