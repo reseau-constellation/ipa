120360e5a68a5edd02559fdf40ed3f492d8949c0
julienmalard <julien.malard@mail.mcgill.ca>
2023-03-09 15:58:50 +0530

    Ajouter nouveau serveur webrtc-star

--- src/sfip/const.ts
+++ src/sfip/const.ts
@@ -1,3 +1,4 @@
 export const ADRESSES_WEBRTC_STAR = [
   "/dns4/arcane-springs-02799.herokuapp.com/tcp/443/wss/p2p-webrtc-star/",
+  "/dns4/serveurwebrtcstarconstellation1-julienmalard.b4a.run/tcp/443/wss/p2p-webrtc-star/",
 ];
