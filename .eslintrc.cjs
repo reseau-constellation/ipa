f178001b7306f515238c777fa832a709d15c60bf
julienmalard <julien.malard@mail.mcgill.ca>
2023-03-31 12:25:25 +0530

    Permettre variables _ non utilisées

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,5 +18,13 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   rules: {
     "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { 
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_"
+      }
+    ]
   },
 };
