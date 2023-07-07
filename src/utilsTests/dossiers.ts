20a3429448b6a6bd68cc7c7c93c128e1660c8032
julienmalard <julien.malard@mail.mcgill.ca>
2023-07-07 09:27:44 +0200

    Nettoyer dossiers

--- src/utilsTests/dossiers.ts
+++ src/utilsTests/dossiers.ts
@@ -6,37 +6,42 @@ export const dossierRessourcesTests = async (): Promise<string> => {
   return path.resolve(path.dirname(""), "src", "utilsTests", "ressources");
 };
 
-export const dossierTempoTests = async (): Promise<string> => {
+export const dossierTempoTests = async (): Promise<{ dossier: string; fEffacer: () => void }> => {
   if (isNode || isElectronMain) {
     const fs = await import("fs");
     const path = await import("path");
     const os = await import("os");
-    return fs.mkdtempSync(path.join(os.tmpdir(), "constl-ipa"));
-  } else {
-    return uuidv4() + "/constl-ipa";
-  }
-};
-
-export const obtDirTempoPourTest = async (
-  nom?: string
-): Promise<{ dossier: string; fEffacer: () => void }> => {
-  if (isNode || isElectronMain) {
-    const fs = await import("fs");
-    const path = await import("path");
     const rimraf = (await import("rimraf")).default;
 
-    const dossierRacine = await dossierTempoTests();
-    const dossier = path.resolve(dossierRacine, (nom || "") + uuidv4());
-    fs.mkdirSync(dossier, { recursive: true });
+    const dossier = fs.mkdtempSync(path.join(os.tmpdir(), "constl-ipa"));
     const fEffacer = () => rimraf.sync(dossier);
     return { dossier, fEffacer };
   } else {
-    const dossier = (await dossierTempoTests()) + "/" + (nom || "") + uuidv4();
+    const dossier = uuidv4() + "/constl-ipa";
     return {
       dossier,
       fEffacer: () => {
         // Rien à faire, je crois
       },
     };
+  }
+};
+
+export const obtDirTempoPourTest = async ({
+  base, nom
+}: {
+  base: string
+  nom?: string
+}): Promise<string> => {
+  if (isNode || isElectronMain) {
+    const fs = await import("fs");
+    const path = await import("path");
+    
+    const dossier = path.resolve(base, (nom || "") + uuidv4());
+    fs.mkdirSync(dossier, { recursive: true });
+    return dossier
+  } else {
+    const dossier = (base) + "/" + (nom || "") + uuidv4();
+    return dossier
   }
 };
