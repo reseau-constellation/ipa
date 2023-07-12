15b155bfafaf5a1175ed986e182c5278ff34c8c3
julienmalard <julien.malard@mail.mcgill.ca>
2023-07-12 15:27:41 +0200

    Solution temporaire pour les dates

--- src/dates.ts
+++ src/dates.ts
@@ -1,25 +1,30 @@
 // Temporaire, avant de crée une nouvelle librairie pour les dates
 export type spécificationHoroDatage = {
-    système: string;
-    val: string;
-    format?: string;
-}
-export const estSpécificationHoroDatage = (val: unknown): val is spécificationHoroDatage => {
-    return !!val && (typeof (val as spécificationHoroDatage).système === 'string' && typeof (val as spécificationHoroDatage).val === 'string')
-}
+  système: string;
+  val: string;
+  format?: string;
+};
+export const estSpécificationHoroDatage = (
+  val: unknown
+): val is spécificationHoroDatage => {
+  return (
+    !!val &&
+    typeof (val as spécificationHoroDatage).système === "string" &&
+    typeof (val as spécificationHoroDatage).val === "string"
+  );
+};
 export class Cholqij {
-
   lireDate(date: spécificationHoroDatage): Date {
     const { système, val } = date;
     switch (système) {
-        case 'dateJS':
-        case 'grégorien': {
-            const date_ = new Date(val)
-            if (isNaN(date_.valueOf())) throw new Error(val)
-            return date_
-        }
-        default:
-            throw new Error(système);
+      case "dateJS":
+      case "grégorien": {
+        const date_ = new Date(val);
+        if (isNaN(date_.valueOf())) throw new Error(val);
+        return date_;
+      }
+      default:
+        throw new Error(système);
     }
   }
 
