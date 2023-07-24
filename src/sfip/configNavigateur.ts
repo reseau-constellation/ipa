17750675b635d44d50d0c624c044512e523f4abd
julienmalard <julien.malard@mail.mcgill.ca>
2023-07-24 14:06:56 +0200

    Websockets cause des drôles d'erreurs dans le navigateur depuis quelques jours

--- src/sfip/configNavigateur.ts
+++ src/sfip/configNavigateur.ts
@@ -1,4 +1,3 @@
-import { webSockets } from "@libp2p/websockets";
 import { webRTCDirect } from "@libp2p/webrtc-direct";
 import { webTransport } from "@libp2p/webtransport";
 import type { create } from "ipfs-core";
@@ -7,7 +6,7 @@ import { ADRESSES_WEBRTC_STAR } from "./const.js";
 
 const config: Parameters<typeof create>[0] = {
   libp2p: {
-    transports: [webSockets(), webRTCDirect(), webTransport()],
+    transports: [webRTCDirect(), webTransport()],
     addresses: {
       listen: ADRESSES_WEBRTC_STAR,
     },
