2cf90c12781da98c6aba78437c91e85ea6774a5d
julienmalard <julien.malard@mail.mcgill.ca>
2023-09-22 10:31:32 +0200

    Progrès contrôlleurs d'accès

--- src/accès/cntrlMod.ts
+++ src/accès/cntrlMod.ts
@@ -1,12 +1,19 @@
-import { EventEmitter } from "events";
 import type {OrbitDB} from "@orbitdb/core";
 import type identityProvider from "orbit-db-identity-provider";
+import * as Block from 'multiformats/block'
+import * as dagCbor from '@ipld/dag-cbor'
+import { sha256 } from 'multiformats/hashes/sha2'
+import { base58btc } from 'multiformats/bases/base58'
 
 import GestionnaireAccès from "@/accès/gestionnaireUtilisateurs.js";
 import { MODÉRATEUR, rôles } from "@/accès/consts.js";
 import type { élémentBdAccès } from "@/accès/types.js";
 
 const type = "controlleur-accès-constellation";
+
+const codec = dagCbor
+const hasher = sha256
+const hashStringEncoding = base58btc
 
 export interface OptionsContrôleurAccèsConstellation {
   premierMod?: string;
@@ -17,42 +24,83 @@ interface OptionsInitContrôleurAccèsConstellation
   premierMod: string;
 }
 
-export default class ContrôleurAccès extends EventEmitter {
-  _premierMod: string;
-  orbitdb: OrbitDB;
-  gestAccès: GestionnaireAccès;
-  dernierAppel: number;
+const PremierModérateur = async ({ storage, type, params }) => {
+  const manifest = {
+    type,
+    ...params
+  }
+  const { cid, bytes } = await Block.encode({ value: manifest, codec, hasher })
+  const hash = cid.toString(hashStringEncoding)
+  await storage.put(hash, bytes)
+  return hash
+}
 
-  constructor(
-    orbitdb: OrbitDB,
-    options: OptionsInitContrôleurAccèsConstellation
-  ) {
-    super();
-    this.orbitdb = orbitdb;
-    this._premierMod = options.premierMod;
+const ContrôleurAccès = ({ write, storage } : { write?: string, storage?: Storage } = { }) => async ({ 
+  orbitdb, identities, address 
+}: { orbitdb: OrbitDB, identities, address?: string}) => {
 
-    this.gestAccès = new GestionnaireAccès(this.orbitdb);
-    this.dernierAppel = Date.now();
+  storage = storage || await ComposedStorage(
+    await LRUStorage({ size: 1000 }),
+    await IPFSBlockStorage({ ipfs: orbitdb.ipfs, pin: true })
+  )
+  write = write || [orbitdb.identity.id]
+
+  let dernierAppel = Date.now();
+  const gestAccès = new GestionnaireAccès(orbitdb);
+
+  if (address) {
+    const manifestBytes = await storage.get(address.replaceAll('/controlleur-accès-constellation/', ''))
+    const { value } = await Block.decode({ bytes: manifestBytes, codec, hasher })
+    write = value.write
+  } else {
+    address = await AccessControlList({ storage, type, params: { write } })
+    address = pathJoin('/', type, address)
   }
 
-  static get type(): string {
-    return type;
+  // Ajouter le premier modérateur
+  await gestAccès.ajouterÉléments([
+    { id: write, rôle: MODÉRATEUR },
+  ]);
+
+  const canAppend = async (
+    entry: LogEntry<élémentBdAccès>,
+    identityProvider: identityProvider
+  ): Promise<boolean> => {
+    const idÉlément = entry.identity.id;
+    const { rôle, id: idAjout } = entry.payload.value;
+    const estUnMod = estUnModérateurPatient(idÉlément);
+    const rôleValide = rôles.includes(rôle);
+
+    const validSig = async () =>
+      identityProvider.verifyIdentity(entry.identity);
+
+    if (rôleValide && (await estUnMod) && (await validSig())) {
+      if (rôle === MODÉRATEUR) {
+        await gestAccès.ajouterÉléments([
+          { id: idAjout, rôle: MODÉRATEUR },
+        ]);
+        dernierAppel = Date.now();
+      }
+      return true;
+    }
+    return false;
   }
 
-  async estUnModérateurPatient(id: string): Promise<boolean> {
+  // À faire : vérifier si toujours nécessaire sur bd-orbite 1,0
+  const estUnModérateurPatient = async (id: string): Promise<boolean> => {
     const PATIENCE = 1000;
 
-    if (await this.gestAccès.estUnModérateur(id)) return true;
+    if (await gestAccès.estUnModérateur(id)) return true;
 
     return new Promise((résoudre) => {
       const partirCrono = () => {
         setTimeout(async () => {
-          const estAutorisé = await this.gestAccès.estUnModérateur(id);
+          const estAutorisé = await gestAccès.estUnModérateur(id);
           if (estAutorisé) {
             résoudre(true);
           } else {
             const maintenant = Date.now();
-            if (maintenant - this.dernierAppel > PATIENCE) {
+            if (maintenant - dernierAppel > PATIENCE) {
               résoudre(false);
             } else {
               partirCrono();
@@ -64,54 +112,14 @@ export default class ContrôleurAccès extends EventEmitter {
     });
   }
 
-  get premierMod(): string {
-    return this._premierMod;
+  return {
+    type,
+    address,
+    write,
+    canAppend,
   }
 
-  async canAppend(
-    entry: LogEntry<élémentBdAccès>,
-    identityProvider: identityProvider
-  ): Promise<boolean> {
-    const idÉlément = entry.identity.id;
-    const { rôle, id: idAjout } = entry.payload.value;
-    const estUnMod = this.estUnModérateurPatient(idÉlément);
-    const rôleValide = rôles.includes(rôle);
-
-    const validSig = async () =>
-      identityProvider.verifyIdentity(entry.identity);
-
-    if (rôleValide && (await estUnMod) && (await validSig())) {
-      if (rôle === MODÉRATEUR) {
-        await this.gestAccès.ajouterÉléments([
-          { id: idAjout, rôle: MODÉRATEUR },
-        ]);
-        this.dernierAppel = Date.now();
-      }
-      return true;
-    }
-    return false;
-  }
-
-  async load(): Promise<void> {
-    // Ajouter le premier modérateur
-    await this.gestAccès.ajouterÉléments([
-      { id: this._premierMod, rôle: MODÉRATEUR },
-    ]);
-  }
-
-  async save(): Promise<{ [key: string]: string }> {
-    const manifest = { premierMod: this.premierMod };
-    return manifest;
-  }
-
-  static async create(
-    orbitdb: OrbitDB,
-    options: OptionsContrôleurAccèsConstellation = {}
-  ): Promise<ContrôleurAccès> {
-    const premierMod = options.premierMod;
-    if (!premierMod) {
-      throw new Error("Contrôle d'accès: premier modérateur requis");
-    }
-    return new ContrôleurAccès(orbitdb, { premierMod });
-  }
 }
+
+ContrôleurAccès.type = type
+export default ContrôleurAccès
