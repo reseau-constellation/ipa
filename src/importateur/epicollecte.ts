7f8306c83eadb88fa506b0075b6db84d791697f6
julienmalard <julien.malard@mail.mcgill.ca>
2023-10-15 19:26:23 +0200

    Nouveau importateur

---
+++ src/importateur/epicollecte.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { élémentDic } from "./json.js";
+
+export const importerDonnéesEpiCollecte = async ({ idProjet, instance = "https://five.epicollect.net" }: {idProjet: string, instance: string}): Promise<élémentDic[]> => {
+    const urlBase = `${instance}/api/export/entries/${idProjet}`
+    const réponseBase = (await axios.get(urlBase)).data;
+    const nPages = réponseBase["meta"]["last_page"]
+    let données: élémentDic[] = [];
+    for (const i in Array(nPages).keys()) {
+        const urlPage = `${instance}/api/export/entries/${idProjet}?page=${i+1}`
+        données = [...données, ...(await axios.get(urlPage)).data["data"]["entries"]];
+    }
+    return données
+}
