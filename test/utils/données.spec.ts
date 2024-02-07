87811002cba733b26a39373302ea9b0a7984f838
julienmalard <julien.malard@mail.mcgill.ca>
2024-02-07 09:15:29 +0100

    Utiliser "/" pour Windows

--- test/utils/données.spec.ts
+++ test/utils/données.spec.ts
@@ -74,13 +74,13 @@ describe("Utils : données", function () {
       it("Les fichiers SFIP sont inclus", async () => {
         expect(zip.files["sfip/"].dir).to.be.true();
         const contenuFichierSFIP1 =
-          await zip.files[path.join("sfip", "fichierSFIP1.txt")].async(
+          await zip.files[["sfip", "fichierSFIP1.txt"].join("/")].async(
             "string",
           );
         expect(contenuFichierSFIP1).to.equal("Je suis le fichier SFIP no. 1.");
 
         const contenuFichierSFIP2 =
-          await zip.files[path.join("sfip", "fichierSFIP2.txt")].async(
+          await zip.files[["sfip", "fichierSFIP2.txt"].join("/")].async(
             "string",
           );
         expect(contenuFichierSFIP2).to.equal("Je suis le fichier SFIP no. 2.");
