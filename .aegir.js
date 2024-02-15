d09b1551fc9efef77e9f3e2d06c376a7c8fa82c3
julienmalard <julien.malard@mail.mcgill.ca>
2024-02-15 16:00:51 +0100

    Progrès libp2p

--- .aegir.js
+++ .aegir.js
@@ -5,6 +5,10 @@ import express from "express";
 import cors from "cors";
 import url from "url";
 import { $ } from "execa";
+import os from "os";
+import esbuildCmd from "esbuild";
+import { mkdtempSync, copyFileSync, appendFileSync, existsSync, lstat, lstatSync, readdirSync, readSync, readFileSync } from "fs";
+import { sync } from "rimraf";
 
 const require = createRequire(import.meta.url);
 
@@ -28,6 +32,7 @@ const esbuild = {
     "electron",
     "env-paths",
     "@libp2p/tcp",
+    "@libp2p/mdns",
   ],
   plugins: [
     {
@@ -53,6 +58,98 @@ const esbuild = {
   ],
 };
 
+const générerServeurRessourcesTests = (opts) => {
+  /**
+   * @type {Express | undefined}
+   */
+  let serveurLocal = undefined;
+
+  if (
+    opts.target.includes("browser") ||
+    opts.target.includes("electron-renderer")
+  ) {
+    const appliExpress = express();
+
+    // Permettre l'accès à partir de l'hôte locale
+    appliExpress.use(
+      cors({
+        origin: function (origin, callback) {
+          if (!origin) {
+            callback(null, true);
+            return;
+          }
+          if (new URL(origin).hostname === "127.0.0.1") {
+            callback(null, true);
+          } else {
+            callback(new Error("Not allowed by CORS"));
+          }
+        },
+      }),
+    );
+    appliExpress.get("/fichier/:nomFichier", function (req, res) {
+      const { nomFichier } = req.params;
+
+      const cheminFichier = path.join(
+        url.fileURLToPath(new URL(".", import.meta.url)),
+        "test",
+        "ressources",
+        decodeURIComponent(nomFichier),
+      );
+
+      res.sendFile(cheminFichier);
+    });
+    serveurLocal = appliExpress.listen(3000);
+  }
+  return serveurLocal;
+};
+
+const lancerSfipDansNavigateur = async (opts) => {
+  const { chromium } = await import("playwright");
+  const navigateur = await chromium.launch();
+
+  const dossierCompilation = mkdtempSync(path.join(os.tmpdir(), "test-constl-"));
+  const  fichierJs = path.join(dossierCompilation, "test.min.js");
+  try {
+    const page = await navigateur.newPage();
+    const globalName = "testnavigsfip";
+    const umdPre = `(function (root, factory) {(typeof module === 'object' && module.exports) ? module.exports = factory() : root.${globalName} = factory()}(typeof self !== 'undefined' ? self : this, function () {`;
+    const umdPost = `return ${globalName}}));`;
+    await esbuildCmd.build({
+      entryPoints: ["dist/test/utils/lancerNœud.js"],
+      bundle: true,
+      format: "iife",
+      conditions: ["production"],
+      outfile: fichierJs,
+      banner: { js: umdPre },
+      footer: { js: umdPost },
+      define: {
+        global: "globalThis",
+        "process.env.NODE_ENV": '"production"',
+      },
+      ...esbuild,
+    });
+
+    page.on("console", (msg) =>
+      console.log("Message de Playwright : ", msg.text()),
+    );
+    const fichierHtml = path.join(dossierCompilation, "lancerNœud.html")
+    
+    copyFileSync(path.join(__dirname, "test", "utils", "lancerNœud.html"), fichierHtml)
+
+    await page.goto(
+      `file://${fichierHtml}`,
+    );
+    await page.getByText("Test").isVisible();
+  } catch (e) {
+    // On arrête pas les tests pour une petite erreur comme ça
+    console.error(e);
+  }
+  return async () => {
+    await navigateur.close();
+    sync(dossierCompilation)
+  };
+};
+
 /** @type {import('aegir').PartialOptions} */
 const options = {
   test: {
@@ -62,68 +159,20 @@ const options = {
       },
     },
     before: async (opts) => {
-      const relai = $`node test/utils/relai.js &`;
+      const relai = undefined; // $`node dist/test/utils/relai.js &`;
 
       // On va lancer une page Constellation pour pouvoir tester la connectivité webrtc avec les navigateurs
-      const { chromium } = await import("playwright");
-      const navigateur = await chromium.launch();
-      try {
-        const page = await navigateur.newPage();
-        await page.goto("https://réseau-constellation.ca");
-      } catch (e) {
-        // On arrête pas les tests pour une petite erreur comme ça
-        console.error(e);
-      }
+      const fermerNavigateur = await lancerSfipDansNavigateur(opts);
 
       // Pour pouvoir accéder les fichiers test dans le navigateur
-      /**
-       * @type {Express | undefined}
-       */
-      let serveurLocal = undefined;
+      const serveurLocal = générerServeurRessourcesTests(opts);
 
-      if (
-        opts.target.includes("browser") ||
-        opts.target.includes("electron-renderer")
-      ) {
-        const appliExpress = express();
-
-        // Permettre l'accès à partir de l'hôte locale
-        appliExpress.use(
-          cors({
-            origin: function (origin, callback) {
-              if (!origin) {
-                callback(null, true);
-                return;
-              }
-              if (new URL(origin).hostname === "127.0.0.1") {
-                callback(null, true);
-              } else {
-                callback(new Error("Not allowed by CORS"));
-              }
-            },
-          }),
-        );
-        appliExpress.get("/fichier/:nomFichier", function (req, res) {
-          const { nomFichier } = req.params;
-
-          const cheminFichier = path.join(
-            url.fileURLToPath(new URL(".", import.meta.url)),
-            "test",
-            "ressources",
-            decodeURIComponent(nomFichier),
-          );
-
-          res.sendFile(cheminFichier);
-        });
-        serveurLocal = appliExpress.listen(3000);
-      }
-
-      return { navigateur, serveurLocal, relai };
+      return { fermerNavigateur, serveurLocal, relai };
     },
     after: async (_, avant) => {
-      await avant.navigateur.close();
+      await avant.fermerNavigateur();
       await avant.serveurLocal?.close();
-      avant.relai.kill();
+      avant.relai?.kill();
     },
   },
   build: {
