2d4d245d13476f72480553d3a95594ede98883b3
julienmalard <julien.malard@mail.mcgill.ca>
2024-02-22 15:59:52 +0100

    Utiliser config tests de @constl/utils-tests

--- .aegir.js
+++ .aegir.js
@@ -1,65 +1,16 @@
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-import { createRequire } from "module";
 import express from "express";
 import cors from "cors";
 import url from "url";
 import { $ } from "execa";
 import os from "os";
 import esbuildCmd from "esbuild";
-import { mkdtempSync, copyFileSync, existsSync } from "fs";
+import { mkdtempSync, copyFileSync } from "fs";
 import { sync } from "rimraf";
-import { dossiers } from "@constl/utils-tests";
+import { dossiers, config } from "@constl/utils-tests";
 
-const require = createRequire(import.meta.url);
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// https://github.com/node-webrtc/node-webrtc/issues/636#issuecomment-774171409
-process.on("beforeExit", (code) => process.exit(code));
-
-// https://github.com/ipfs/aegir/blob/master/md/migration-to-v31.md
-const esbuild = {
-  // this will inject all the named exports from 'node-globals.js' as globals
-  inject: [path.join(__dirname, "./scripts/node-globals.js")],
-  external: [
-    "fs",
-    "path",
-    "os",
-    "chokidar",
-    "url",
-    "zlib",
-    "rimraf",
-    "electron",
-    "env-paths",
-    "@libp2p/tcp",
-    "@libp2p/mdns",
-  ],
-  plugins: [
-    {
-      name: "node built ins", // this will make the bundler resolve node builtins to the respective browser polyfill
-      setup(build) {
-        build.onResolve({ filter: /^stream$/ }, () => {
-          return { path: require.resolve("stream-browserify") };
-        });
-        build.onResolve({ filter: /^os$/ }, () => {
-          return { path: require.resolve("os-browserify") };
-        });
-        build.onResolve({ filter: /^node\:process$/ }, () => {
-          return { path: require.resolve("process/browser") };
-        });
-        build.onResolve({ filter: /^crypto$/ }, () => {
-          return { path: require.resolve("crypto-browserify") };
-        });
-        build.onResolve({ filter: /^fs/ }, () => {
-          return { path: require.resolve("browserify-fs") };
-        });
-      },
-    },
-  ],
-};
-
-const générerServeurRessourcesTests = (opts) => {
+const générerServeurRessourcesTests = async (opts) => {
   /**
    * @type {Express | undefined}
    */
@@ -101,7 +52,7 @@ const générerServeurRessourcesTests = (opts) => {
     });
     serveurLocal = appliExpress.listen(3000);
   }
-  return serveurLocal;
+  return async () =>serveurLocal.close();
 };
 
 const lancerSfipDansNode = async (opts) => {
@@ -133,6 +84,7 @@ const lancerSfipDansNavigateur = async (opts) => {
     const globalName = "testnavigsfip";
     const umdPre = `(function (root, factory) {(typeof module === 'object' && module.exports) ? module.exports = factory() : root.${globalName} = factory()}(typeof self !== 'undefined' ? self : this, function () {`;
     const umdPost = `return ${globalName}}));`;
+    const configEsbuild = await config.obtConfigEsbuild();
     await esbuildCmd.build({
       entryPoints: ["dist/test/utils/lancerNœud.js"],
       bundle: true,
@@ -145,11 +97,12 @@ const lancerSfipDansNavigateur = async (opts) => {
         global: "globalThis",
         "process.env.NODE_ENV": '"production"',
       },
-      ...esbuild,
+      ...configEsbuild,
     });
 
     const fichierHtml = path.join(dossierCompilation, "lancerNœud.html");
 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
     copyFileSync(
       path.join(__dirname, "test", "utils", "lancerNœud.html"),
       fichierHtml,
@@ -167,39 +120,50 @@ const lancerSfipDansNavigateur = async (opts) => {
   };
 };
 
-/** @type {import('aegir').PartialOptions} */
-const options = {
-  test: {
-    browser: {
-      config: {
-        buildConfig: esbuild,
-      },
-    },
-    before: async (opts) => {
-      const relai = (opts.target.includes("browser") || opts.target.includes("electron-renderer")) ? $`node dist/test/utils/relai.js &` : undefined;
+const avantTest = async (opts) => {
 
-      // On va lancer une page Constellation pour pouvoir tester la connectivité webrtc avec les navigateurs
-      const fermerNavigateur = await lancerSfipDansNavigateur(opts);
+  // On va lancer une page Constellation pour pouvoir tester la connectivité webrtc avec les navigateurs
+  const fermerNavigateur = await lancerSfipDansNavigateur(opts);
 
-      // Et une sur Node.js pour pouvoir tester la connectivité avec Node
-      const fermerNode = await lancerSfipDansNode(opts);
+  // Et une sur Node.js pour pouvoir tester la connectivité avec Node
+  const fermerNode = await lancerSfipDansNode(opts);
 
-      // Pour pouvoir accéder les fichiers test dans le navigateur
-      const serveurLocal = générerServeurRessourcesTests(opts);
+  // Pour pouvoir accéder les fichiers test dans le navigateur
+  const fermerServeurLocal = await générerServeurRessourcesTests(opts);
 
-      return { fermerNavigateur, fermerNode, serveurLocal, relai };
-    },
-    after: async (_, avant) => {
-      console.log("On ferme aegir");
-      await avant.fermerNavigateur();
-      await avant.fermerNode();
-      await avant.serveurLocal?.close();
-      avant.relai?.kill();
-    },
-  },
-  build: {
-    config: esbuild,
-  },
+  return { fermerNavigateur, fermerNode, fermerServeurLocal };
 };
 
-export default options;
+const aprèsTest = async (_, avant) => {
+  await avant.fermerNavigateur();
+  await avant.fermerNode();
+  await avant.fermerServeurLocal();
+};
+
+const générerConfigÆgirFinal = async () => {
+  const configÆgir = await config.générerConfigÆgir();
+  
+  const avantTestDéfaut = configÆgir.test.before
+  configÆgir.test.before = async (opts) => {
+    const retourAvantTestDéfaut = await avantTestDéfaut(opts);
+    const retourAvantTest = await avantTest(opts);
+    return {
+      ...retourAvantTestDéfaut,
+      ...retourAvantTest,
+    }
+  }
+
+  const aprèsTestDéfaut = configÆgir.test.after;
+  configÆgir.test.after = async (_, avant) => {
+    const retourAprèsTestDéfaut = await aprèsTestDéfaut(_, avant);
+    const retourAprèsTest = await aprèsTest(_, avant);
+    return {
+      ...retourAprèsTestDéfaut,
+      ...retourAprèsTest,
+    }
+  }
+
+  return configÆgir;
+};
+
+export default générerConfigÆgirFinal();
