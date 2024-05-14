04f53fc3940205368b0dc7f66402f6e5e603ca54
julienmalard <julien.malard@mail.mcgill.ca>
2024-05-14 17:44:18 -0400

    Plus besoin des langues cibles explicites

--- docu/scripts/compilateur.ts
+++ docu/scripts/compilateur.ts
@@ -7,7 +7,6 @@ export const obtCompilateur = async () => {
 
   const compilateur = new Compilateur({
     languePrincipale: "fr",
-    languesCibles: ["த", "cst", "kaq", "हिं", "فا", "ગુ", "తె", "ಕ", "മ"],
     dossierSource: "src",
     dossierTraductions: "traducs",
     configVitePress,
