1d3e41a58b3ba274edaf384c4d094bc088941f67
julienmalard <julien.malard@mail.mcgill.ca>
2024-06-18 14:56:08 +0200

    Migrer eslint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.js", "**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
