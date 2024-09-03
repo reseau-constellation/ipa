ae2a201c0db8f3e2dc83a7cdf3244f9edac0f743
julienmalard <julien.malard@mail.mcgill.ca>
2024-09-03 13:03:43 +0530

    Test mise à jour

--- src/sfip/const.ts
+++ src/sfip/const.ts
@@ -1,10 +1,11 @@
 // Voir https://github.com/alexeyvolkoff/orbitdb-example/blob/main/relay.js
 // https://github.com/libp2p/js-libp2p-example-circuit-relay
 export const ADRESSES_NŒUDS_RELAI = [
-  "/ip4/164.90.222.145/udp/9090/webrtc-direct/certhash/uEiAJOkKT64u6jmXV5YxncCoER5WXSO2HYE23Xpap651xMw/p2p/12D3KooWJ7P1yeoxB5mq3TwQh8YgmVhankjtT4rsVGZPUyf617aR",
   "/ip4/164.90.222.145/udp/9091/quic-v1/p2p/12D3KooWJ7P1yeoxB5mq3TwQh8YgmVhankjtT4rsVGZPUyf617aR",
   "/ip4/164.90.222.145/tcp/9092/p2p/12D3KooWJ7P1yeoxB5mq3TwQh8YgmVhankjtT4rsVGZPUyf617aR",
 ];
+
+export const IDS_NŒUDS_RELAI = ['12D3KooWJ7P1yeoxB5mq3TwQh8YgmVhankjtT4rsVGZPUyf617aR']
 
 export const ADRESSES_NŒUDS_RELAI_LOCAL = [
   "/ip4/127.0.0.1/udp/9090/webrtc-direct/certhash/uEiAWCRG30KUzLUuWYZ9HZNcFBHt8uLjLRB2xlLW_L_8lQg/p2p/12D3KooWKeJNXytogRbQPvYrFxXAaDGaJUfysrPeGbbEBDM78iTu",
