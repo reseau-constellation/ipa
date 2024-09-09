44c74f08c742219c6166dc6c1d659fe9c66c92b2
julienmalard <julien.malard@mail.mcgill.ca>
2024-09-09 17:25:04 +0530

    Éviter collision potentielle avec id mot-clef

--- test/recherche/projet.spec.ts
+++ test/recherche/projet.spec.ts
@@ -649,7 +649,7 @@ describe("Rechercher projets", function () {
 
       const val = await résultatMotsClef.attendreQue(
         (x) =>
-          (x.info as infoRésultatRecherche<infoRésultatTexte>).de === "motClef",
+          (x.info as infoRésultatRecherche<infoRésultatTexte>).de === "motClef" && x.info.type === "résultat" && x.info.de === "nom",
       );
       expect(val).to.deep.equal(réfRés);
     });
