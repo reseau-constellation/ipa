e056cbe877db73cb2995bbc487b7ed2268e057aa
julienmalard <julien.malard@mail.mcgill.ca>
2024-11-08 11:14:51 +0530

    Ajuster console.log

--- test/utils/lancerNœud.ts
+++ test/utils/lancerNœud.ts
@@ -11,41 +11,13 @@ const dossier = isBrowser
     : "./testSfip";
 
 initSFIP({ dossier, configLibp2p: {} }).then(async (sfip) => {
+  // Important ! Ne pas effacer cet appel à `console.log` 
   console.log(
     "SFIP initialisé avec id de nœud :",
     sfip.libp2p.peerId.toString(),
   );
-  console.log(
-    "nos adresses: ",
-    sfip.libp2p.peerId.toString(),
-    sfip.libp2p.getMultiaddrs().map((ma) => ma.toString()),
-  );
-  sfip.libp2p.addEventListener("peer:connect", async () => {
-    /*const pairs = sfip.libp2p.getPeers();
-    const connexions = sfip.libp2p.getConnections();
-    console.log(
-      "pairs : ",
-      pairs.map((p) => p.toString()),
-    );
-    console.log(
-      "connexions : ",
-      JSON.stringify(
-        connexions.map((c) => [
-          c.remotePeer.toString(),
-          c.remoteAddr.toString(),
-        ]),
-        undefined,
-        2,
-      ),
-    );
-    */
-  });
   sfip.libp2p.services.pubsub.subscribe(CANAL_TEST);
   sfip.libp2p.services.pubsub.addEventListener("gossipsub:message", (m) => {
-    /*console.log(
-      m.detail.msg.topic,
-      new TextDecoder().decode(m.detail.msg.data),
-    );*/
     if (m.detail.msg.topic === CANAL_TEST) {
       const idPair = sfip.libp2p.peerId.toString();
       const message = JSON.parse(
