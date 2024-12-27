aaf24800a653492d1c3a2b325f0168f28c34ec46
julienmalard <julien.malard@mail.mcgill.ca>
2024-12-28 01:11:38 +0530

    Test (non activé)

--- test/client.spec.ts
+++ test/client.spec.ts
@@ -343,6 +343,43 @@ if (isNode || isElectronMain) {
         );
       });
     });
+
+    describe.skip("Même dossier - serveur local", async () => {
+      let fermerServeur: schémaFonctionOublier;
+      let port: number;
+
+      let dossier: string;
+      let fEffacer: () => void;
+
+      before(async () => {
+        /*const {lancerServeur} = await import("@constl/serveur");
+        ({ dossier, fEffacer } = await dossiers.dossierTempo());
+        ({fermerServeur, port} = await lancerServeur({ optsConstellation: { dossier }}));
+        */
+      });
+
+      after(async () => {
+        await fermerServeur();
+        try {
+          fEffacer?.();
+        } catch (e) {
+          if ((isNode || isElectronMain) && process.platform === "win32") {
+            console.log("On ignore ça sur Windows\n", e);
+            return;
+          } else {
+            throw e;
+          }
+        }
+      });
+
+      it("Erreur pour la deuxième instance, avec port disponible", async () => {
+        const constl2 = créerConstellation({ dossier });
+        await expect(constl2.obtIdCompte()).to.be.rejectedWith(
+          `Ce compte est déjà lancé, et le serveur local est disponible sur le port ${port}.`
+        );
+        await fermerServeur();
+      });
+    });
   });
 
   describe("Fonctionalités client", function () {
