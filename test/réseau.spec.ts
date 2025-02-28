d27e3a441754b87fd795b8db9634e9776f3470e1
julienmalard <julien.malard@mail.mcgill.ca>
2025-02-28 22:39:31 +0100

    Réactiver tests

--- test/réseau.spec.ts
+++ test/réseau.spec.ts
@@ -59,7 +59,7 @@ async function toutPréparer(n: number) {
 }
 
 if (isNode || isElectronMain) {
-  describe.only("Réseau", function () {
+  describe("Réseau", function () {
     describe("Suivre en ligne", function () {
       let fOublierConstls: () => Promise<void>;
       let idsComptes: string[];
@@ -2438,7 +2438,7 @@ if (isNode || isElectronMain) {
       });
     });
 
-    describe.only("Messages", function () {
+    describe("Messages", function () {
       let fOublierConstls: () => Promise<void>;
       let idsDispositifs: string[];
       let idsComptes: string[];
