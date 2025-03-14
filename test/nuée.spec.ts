af00fbcc3e0365f199da27cfe38080dc7b9f72d9
julienmalard <julien.malard@mail.mcgill.ca>
2025-03-14 22:21:35 +0100

    Réactiver tests

--- test/nuée.spec.ts
+++ test/nuée.spec.ts
@@ -64,7 +64,7 @@ const idsCorrespondantes = async (
     .lengthOf(nIdsDésirées);
 };
 
-describe.only("Nuées", function () {
+describe("Nuées", function () {
   describe("Tests individuels", function () {
     let fOublierClients: () => Promise<void>;
     let clients: Constellation[];
