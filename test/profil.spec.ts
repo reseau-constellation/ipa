fe45587a01f360f5e678f7386f29d53b3082d485
julienmalard <julien.malard@mail.mcgill.ca>
2025-06-24 13:26:54 +0530

    Simplifier tests

--- test/profil.spec.ts
+++ test/profil.spec.ts
@@ -1,12 +1,12 @@
 import {
-  attente as utilsTestAttente,
   constellation as utilsTestConstellation,
 } from "@constl/utils-tests";
 import { expect } from "aegir/chai";
 import { type Constellation, créerConstellation } from "@/index.js";
 import { MAX_TAILLE_IMAGE } from "@/profil.js";
 import { obtRessourceTest } from "./ressources/index.js";
-import type { schémaFonctionOublier } from "@/types.js";
+import type { TraducsNom, schémaFonctionOublier } from "@/types.js";
+import { obtenir } from "@constl/utils-ipa";
 
 describe("Profil", function () {
   let fOublierClients: () => Promise<void>;
@@ -29,24 +29,18 @@ describe("Profil", function () {
   describe("Initialiser profil", function () {
     let fOublier: schémaFonctionOublier;
 
-    const résultatInitialisé = new utilsTestAttente.AttendreRésultat<boolean>();
-
-    before(async () => {
-      fOublier = await client.profil.suivreInitialisé({
-        f: (c) => résultatInitialisé.mettreÀJour(c),
-      });
-    });
-
     it("Pas initialisé pour commencer", async () => {
-      const courriel = await résultatInitialisé.attendreQue(
-        (x) => x !== undefined,
-      );
-      expect(courriel).to.be.false();
+      const initialisé = await obtenir(({siDéfini}) => client.profil.suivreInitialisé({
+        f: siDéfini(),
+      }));
+      expect(initialisé).to.be.false();
     });
 
     it("Initialiser", async () => {
       await client.profil.initialiser();
-      const initialisé = await résultatInitialisé.attendreQue((x) => x);
+      const initialisé = await obtenir(({si}) => client.profil.suivreInitialisé({
+        f: si(x=>x !== false),
+      }));
       expect(initialisé).to.be.true();
     });
 
@@ -58,33 +52,28 @@ describe("Profil", function () {
   describe("Courriels", function () {
     let fOublier: schémaFonctionOublier;
 
-    const résultatCourriel = new utilsTestAttente.AttendreRésultat<
-      string | null
-    >();
     const COURRIEL = "தொடர்பு@லஸ்ஸி.இந்தியா";
 
-    before(async () => {
-      fOublier = await client.profil.suivreCourriel({
-        f: (c) => résultatCourriel.mettreÀJour(c),
-      });
-    });
-
     it("Pas de courriel pour commencer", async () => {
-      const courriel = await résultatCourriel.attendreQue(
-        (c) => c !== undefined,
-      );
+      const courriel = await await obtenir(({siDéfini}) => client.profil.suivreCourriel({
+        f: siDéfini(),
+      }));
       expect(courriel).to.be.null();
     });
 
     it("Ajouter un courriel", async () => {
       await client.profil.sauvegarderCourriel({ courriel: COURRIEL });
-      const courriel = await résultatCourriel.attendreExiste();
+      const courriel = await obtenir(({siPasNul}) => client.profil.suivreInitialisé({
+        f: siPasNul(),
+      }));
       expect(courriel).to.equal(COURRIEL);
     });
 
     it("Effacer le courriel", async () => {
       await client.profil.effacerCourriel();
-      const courriel = await résultatCourriel.attendreQue((c) => !c);
+      const courriel = await obtenir(({siNul}) => client.profil.suivreInitialisé({
+        f: siNul(),
+      }));
       expect(courriel).to.be.null();
     });
 
@@ -94,25 +83,11 @@ describe("Profil", function () {
   });
 // nom
   describe("Noms", function () {
-    const rés = new utilsTestAttente.AttendreRésultat<{
-      [key: string]: string;
-    }>();
-    let fOublier: schémaFonctionOublier;
-
-    before(async () => {
-      fOublier = await client.profil.suivreNoms({
-        f: (n) => rés.mettreÀJour(n),
-      });
-    });
-
-    after(async () => {
-      if (fOublier) await fOublier();
-      rés.toutAnnuler();
-    });
-
     it("Pas de noms pour commencer", async () => {
-      const val = await rés.attendreExiste();
-      expect(Object.keys(val)).to.be.empty();
+      const noms = await obtenir<TraducsNom>(({siDéfini})=>client.profil.suivreNoms({
+        f: siDéfini(),
+      }));
+      expect(Object.keys(noms)).to.be.empty();
     });
 
     it("Ajouter un nom", async () => {
@@ -120,15 +95,19 @@ describe("Profil", function () {
         langue: "fr",
         nom: "Julien Malard-Adam",
       });
-      const val = await rés.attendreQue((x) => Object.keys(x).length > 0);
-      expect(val.fr).to.equal("Julien Malard-Adam");
+      const noms = await obtenir<TraducsNom>(({siPasVide})=>client.profil.suivreNoms({
+        f: siPasVide(),
+      }));
+      expect(noms.fr).to.equal("Julien Malard-Adam");
 
       await client.profil.sauvegarderNom({
         langue: "த",
         nom: "ஜூலீஎன்",
       });
-      const val2 = await rés.attendreQue((x) => Object.keys(x).length > 1);
-      expect(val2.த).to.equal("ஜூலீஎன்");
+      const nomsMaintenant = await obtenir<TraducsNom>(({siPasVide})=>client.profil.suivreNoms({
+        f: siPasVide(),
+      }));
+      expect(nomsMaintenant.த).to.equal("ஜூலீஎன்");
     });
 
     it("Changer un nom", async () => {
@@ -136,82 +115,72 @@ describe("Profil", function () {
         langue: "த",
         nom: "ம.-ஆதான் ஜூலீஎன்",
       });
-      const val = await rés.attendreQue((x) => x.த !== "ஜூலீஎன்");
-      expect(val.த).to.equal("ம.-ஆதான் ஜூலீஎன்");
+      const noms = await obtenir<TraducsNom>(({si})=>client.profil.suivreNoms({
+        f: si((x) => x.த !== "ஜூலீஎன்"),
+      }));
+      expect(noms.த).to.equal("ம.-ஆதான் ஜூலீஎன்");
     });
 
     it("Effacer un nom", async () => {
       await client.profil.effacerNom({ langue: "fr" });
-      const val = await rés.attendreQue((x) => Object.keys(x).length <= 1);
-      expect(val).to.deep.equal({ த: "ம.-ஆதான் ஜூலீஎன்" });
+      const noms = await obtenir<TraducsNom>(({si})=>client.profil.suivreNoms({
+        f: si((x) => Object.keys(x).length <= 1),
+      }));
+      expect(noms).to.deep.equal({ த: "ம.-ஆதான் ஜூலீஎன்" });
     });
   });
 // bio
 describe("Bios", function () {
-  const rés = new utilsTestAttente.AttendreRésultat<{
-    [key: string]: string;
-  }>();
-  let fOublier: schémaFonctionOublier;
-
-  before(async () => {
-    fOublier = await client.profil.suivreBios({
-      f: (n) => rés.mettreÀJour(n),
-    });
-  });
-
-  after(async () => {
-    if (fOublier) await fOublier();
-    rés.toutAnnuler();
-  });
-  
-
 
   it("Pas de bios pour commencer", async () => {
-    const val = await rés.attendreExiste();
-    expect(Object.keys(val)).to.be.empty();
+    const bios = await obtenir<TraducsNom>(({siDéfini})=>client.profil.suivreBios({
+      f: siDéfini(),
+    }));
+    expect(Object.keys(bios)).to.be.empty();
   });
   
 
-  it("Ajouter un bio", async () => {
+  it("Ajouter une bio", async () => {
     await client.profil.sauvegarderBio({
       langue: "fr",
       bio: "Julien Malard-Adam",
     });
-    const val = await rés.attendreQue((x) => Object.keys(x).length > 0);
-    expect(val.fr).to.equal("Julien Malard-Adam");
+    const bios = await obtenir<TraducsNom>(({siPasVide})=>client.profil.suivreBios({
+      f: siPasVide(),
+    }));
+    expect(bios.fr).to.equal("Julien Malard-Adam");
 
     await client.profil.sauvegarderBio({
-      langue: "मैथिली",
+      langue: "मै",
       bio: "अहाँ सिखैत रहू।",  
     });
-    const val2 = await rés.attendreQue((x) => Object.keys(x).length > 1);
-    expect(val2.मैथिली).to.equal("अहाँ सिखैत रहू।");
+    const bios2 = await obtenir<TraducsNom>(({si})=>client.profil.suivreBios({
+      f: si((x) => Object.keys(x).length > 1),
+    }));
+    expect(bios2.मैथिली).to.equal("अहाँ सिखैत रहू।");
   });
 
-  it("Changer un bio", async () => {
+  it("Changer une bio", async () => {
     await client.profil.sauvegarderBio({
-      langue: "मैथिली",
+      langue: "मै",
       bio: "अहाँ सिखैत रहू।",
     });
-    const val = await rés.attendreQue((x) => x.த !== "अहाँ सिखैत रहू।");
-    expect(val.मैथिली).to.equal("अहाँ सिखैत रहू।");
+    const bios = await obtenir<TraducsNom>(({si})=>client.profil.suivreBios({
+      f: si((x) => x.मै !== "अहाँ सिखैत रहू।"),
+    }));
+    expect(bios.मैथिली).to.equal("अहाँ सिखैत रहू।");
   });
 
-  it("Effacer un bio", async () => {
+  it("Effacer une bio", async () => {
     await client.profil.effacerBio({ langue: "fr" });
-    const val = await rés.attendreQue((x) => Object.keys(x).length <= 1);
-    expect(val).to.deep.equal({ मैथिली: "अहाँ सिखैत रहू।" });
+    const bios = await obtenir<TraducsNom>(({si})=>client.profil.suivreBios({
+      f: si((x) => Object.keys(x).length <= 1),
+    }));
+    expect(bios).to.deep.equal({ मैथिली: "अहाँ सिखैत रहू।" });
   });
 });
 
   describe("Images", function () {
-    const rés = new utilsTestAttente.AttendreRésultat<{
-      image: Uint8Array;
-      idImage: string;
-    } | null>();
-
-    let fOublier: schémaFonctionOublier;
-
     let IMAGE: Uint8Array;
 
     before(async () => {
@@ -220,34 +189,32 @@ describe("Bios", function () {
       });
     });
 
-    before(async () => {
-      fOublier = await client.profil.suivreImage({
-        f: (i) => rés.mettreÀJour(i),
-      });
-    });
-
-    after(async () => {
-      if (fOublier) await fOublier();
-      rés.toutAnnuler();
-    });
-
     it("Pas d'image pour commencer", async () => {
-      const val = await rés.attendreQue((x) => x === null);
-      expect(val).to.be.null();
+      const image = await obtenir(({siDéfini})=>client.profil.suivreImage({
+        f: siDéfini(),
+      }));
+      expect(image).to.be.null();
     });
 
     it("Ajouter une image", async () => {
       await client.profil.sauvegarderImage({
         image: { contenu: IMAGE, nomFichier: "logo.svg" },
       });
-      const val = await rés.attendreExiste();
-      expect(val?.image).to.deep.equal(new Uint8Array(IMAGE));
+      const image = await obtenir<{
+        image: Uint8Array;
+        idImage: string;
+    } | null>(({siPasNul})=>client.profil.suivreImage({
+        f: siPasNul(),
+      }));
+      expect(image?.image).to.deep.equal(new Uint8Array(IMAGE));
     });
 
     it("Effacer l'image", async () => {
       await client.profil.effacerImage();
-      const val = await rés.attendreQue((x) => x === null);
-      expect(val).to.be.null();
+      const image = await obtenir(({siNul})=>client.profil.suivreImage({
+        f: siNul(),
+      }));
+      expect(image).to.be.null();
     });
 
     it("Ajouter une image trop grande", async () => {
