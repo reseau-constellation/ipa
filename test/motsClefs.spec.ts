14fd3085aa8f36b109d6186e5e346dfeb57dcee7
julienmalard <julien.malard@mail.mcgill.ca>
2025-06-25 09:59:42 +0530

    Simplifier tests

--- test/motsClefs.spec.ts
+++ test/motsClefs.spec.ts
@@ -1,16 +1,13 @@
 import { expect } from "aegir/chai";
 
-import {
-  attente,
-  attente as utilsTestAttente,
-  constellation as utilsTestConstellation,
-} from "@constl/utils-tests";
+import { constellation as utilsTestConstellation } from "@constl/utils-tests";
+import { obtenir } from "@constl/utils-ipa";
 import { créerConstellation, type Constellation } from "@/index.js";
-import type { schémaFonctionOublier } from "@/types.js";
+import { TraducsNom, type schémaFonctionOublier } from "@/types.js";
 
 const { créerConstellationsTest } = utilsTestConstellation;
 
-describe("Mots-clefs", function () {
+describe.only("Mots-clefs", function () {
   let fOublierClients: () => Promise<void>;
   let clients: Constellation[];
   let client: Constellation;
@@ -30,100 +27,95 @@ describe("Mots-clefs", function () {
 
   describe("Création", function () {
     let idMotClef: string;
-    let fOublier: schémaFonctionOublier;
 
-    const motsClefs = new utilsTestAttente.AttendreRésultat<string[]>();
-
-    before(async () => {
-      fOublier = await client.motsClefs.suivreMotsClefs({
-        f: (x) => motsClefs.mettreÀJour(x),
-      });
-    });
-
-    after(async () => {
-      if (fOublier) await fOublier();
-    });
     it("Pas de mots-clefs pour commencer", async () => {
-      const val = await motsClefs.attendreExiste();
+      const motsClefs = await obtenir<string[]>(({ siDéfini }) =>
+        client.motsClefs.suivreMotsClefs({
+          f: siDéfini(),
+        }),
+      );
 
-      expect(val).to.be.an.empty("array");
+      expect(motsClefs).to.be.an.empty("array");
     });
+
     it("Créer des mots-clefs", async () => {
       idMotClef = await client.motsClefs.créerMotClef();
-      const val = await motsClefs.attendreQue((x) => !!x.length);
+      const motsClefs = await obtenir<string[]>(({ siPasVide }) =>
+        client.motsClefs.suivreMotsClefs({
+          f: siPasVide(),
+        }),
+      );
 
-      expect(Array.isArray(val)).to.be.true();
-      expect(val.length).to.equal(1);
+      expect(Array.isArray(motsClefs)).to.be.true();
+      expect(motsClefs.length).to.equal(1);
     });
     it("Effacer un mot-clef", async () => {
       await client.motsClefs.effacerMotClef({ idMotClef });
-      const val = await motsClefs.attendreQue((x) => !x.length);
+      const motsClefs = await obtenir<string[]>(({ siVide }) =>
+        client.motsClefs.suivreMotsClefs({
+          f: siVide(),
+        }),
+      );
 
-      expect(val).to.be.an.empty("array");
+      expect(motsClefs).to.be.an.empty("array");
     });
   });
 
   describe("Mes mots-clefs", function () {
     let idMotClef: string;
-    let fOublier: schémaFonctionOublier;
-
-    const mesMotsClefs = new utilsTestAttente.AttendreRésultat<string[]>();
 
     before(async () => {
       idMotClef = await client.motsClefs.créerMotClef();
-      fOublier = await client.motsClefs.suivreMotsClefs({
-        f: (mc) => mesMotsClefs.mettreÀJour(mc),
-      });
-    });
-
-    after(async () => {
-      if (fOublier) await fOublier();
     });
 
     it("Le mot-clef est déjà ajouté", async () => {
-      const val = await mesMotsClefs.attendreQue((x) => !!x.length);
+      const motsClefs = await obtenir<string[]>(({ siPasVide }) =>
+        client.motsClefs.suivreMotsClefs({
+          f: siPasVide(),
+        }),
+      );
 
-      expect(val).to.contain(idMotClef);
+      expect(motsClefs).to.contain(idMotClef);
     });
 
     it("Enlever de mes mots-clefs", async () => {
       await client.motsClefs.enleverDeMesMotsClefs({ idMotClef });
-      const val = await mesMotsClefs.attendreQue((x) => !x.length);
+      const motsClefs = await obtenir<string[]>(({ siVide }) =>
+        client.motsClefs.suivreMotsClefs({
+          f: siVide(),
+        }),
+      );
 
-      expect(val).not.to.contain(idMotClef);
+      expect(motsClefs).not.to.contain(idMotClef);
     });
 
     it("Ajouter à mes mots-clefs", async () => {
       await client.motsClefs.ajouterÀMesMotsClefs({ idMotClef });
-      const val = await mesMotsClefs.attendreQue((x) => !!x.length);
+      const motsClefs = await obtenir<string[]>(({ siPasVide }) =>
+        client.motsClefs.suivreMotsClefs({
+          f: siPasVide(),
+        }),
+      );
 
-      expect(val).to.contain(idMotClef);
+      expect(motsClefs).to.contain(idMotClef);
     });
   });
 
   describe("Noms", function () {
-    const rés = new utilsTestAttente.AttendreRésultat<{
-      [key: string]: string;
-    }>();
     let idMotClef: string;
-    let fOublier: schémaFonctionOublier;
 
     before(async () => {
       idMotClef = await client.motsClefs.créerMotClef();
-      fOublier = await client.motsClefs.suivreNomsMotClef({
-        idMotClef,
-        f: (n) => rés.mettreÀJour(n),
-      });
-    });
-
-    after(async () => {
-      if (fOublier) await fOublier();
-      rés.toutAnnuler();
     });
 
     it("Pas de noms pour commencer", async () => {
-      const val = await rés.attendreExiste();
-      expect(Object.keys(val).length).to.equal(0);
+      const noms = await obtenir<TraducsNom>(({ siDéfini }) =>
+        client.motsClefs.suivreNomsMotClef({
+          idMotClef,
+          f: siDéfini(),
+        }),
+      );
+      expect(Object.keys(noms).length).to.equal(0);
     });
 
     it("Ajouter un nom", async () => {
@@ -132,8 +124,13 @@ describe("Mots-clefs", function () {
         langue: "fr",
         nom: "Hydrologie",
       });
-      const val = await rés.attendreQue((x) => Object.keys(x).length > 0);
-      expect(val.fr).to.equal("Hydrologie");
+      const noms = await obtenir<TraducsNom>(({ siPasVide }) =>
+        client.motsClefs.suivreNomsMotClef({
+          idMotClef,
+          f: siPasVide(),
+        }),
+      );
+      expect(noms.fr).to.equal("Hydrologie");
     });
 
     it("Ajouter des noms", async () => {
@@ -144,8 +141,14 @@ describe("Mots-clefs", function () {
           हिं: "जल विज्ञान",
         },
       });
-      await rés.attendreQue((x) => Object.keys(x).length >= 3);
-      expect(rés.val).to.deep.equal({
+      const noms = await obtenir<TraducsNom>(({ si }) =>
+        client.motsClefs.suivreNomsMotClef({
+          idMotClef,
+          f: si((x) => Object.keys(x).length >= 3),
+        }),
+      );
+
+      expect(noms).to.deep.equal({
         த: "நீரியல்",
         हिं: "जल विज्ञान",
         fr: "Hydrologie",
@@ -159,8 +162,13 @@ describe("Mots-clefs", function () {
         nom: "hydrologie",
       });
 
-      await rés.attendreQue((x) => x["fr"] == "hydrologie");
-      expect(rés.val?.fr).to.equal("hydrologie");
+      const noms = await obtenir<TraducsNom>(({ si }) =>
+        client.motsClefs.suivreNomsMotClef({
+          idMotClef,
+          f: si((x) => x["fr"] === "hydrologie"),
+        }),
+      );
+      expect(noms.fr).to.equal("hydrologie");
     });
 
     it("Effacer un nom", async () => {
@@ -168,8 +176,13 @@ describe("Mots-clefs", function () {
         idMotClef,
         langue: "fr",
       });
-      await rés.attendreQue((x) => !Object.keys(x).includes("fr"));
-      expect(rés.val).to.deep.equal({
+      const noms = await obtenir<TraducsNom>(({ si }) =>
+        client.motsClefs.suivreNomsMotClef({
+          idMotClef,
+          f: si((x) => !Object.keys(x).includes("fr")),
+        }),
+      );
+      expect(noms).to.deep.equal({
         த: "நீரியல்",
         हिं: "जल विज्ञान",
       });
@@ -177,23 +190,9 @@ describe("Mots-clefs", function () {
   });
 
   describe("Copier mots-clefs", function () {
-    let motsClefs: string[];
-
     let idMotClef2: string;
 
-    const noms = new attente.AttendreRésultat<{ [key: string]: string }>();
-    const descriptions = new attente.AttendreRésultat<{
-      [key: string]: string;
-    }>();
-    const fsOublier: schémaFonctionOublier[] = [];
-
     before(async () => {
-      fsOublier.push(
-        await client.motsClefs.suivreMotsClefs({
-          f: (x) => (motsClefs = x),
-        }),
-      );
-
       const idMotClef = await client.motsClefs.créerMotClef();
       await client.motsClefs.sauvegarderNomsMotClef({
         idMotClef,
@@ -214,39 +213,36 @@ describe("Mots-clefs", function () {
       idMotClef2 = await client.motsClefs.copierMotClef({
         idMotClef,
       });
-      fsOublier.push(
-        await client.motsClefs.suivreNomsMotClef({
-          idMotClef: idMotClef2,
-          f: (x) => noms.mettreÀJour(x),
-        }),
-      );
-      fsOublier.push(
-        await client.motsClefs.suivreDescriptionsMotClef({
-          idMotClef: idMotClef2,
-          f: (x) => descriptions.mettreÀJour(x),
-        }),
-      );
-    });
-
-    after(async () => {
-      await Promise.allSettled(fsOublier.map((f) => f()));
-      noms.toutAnnuler();
-      descriptions.toutAnnuler();
     });
 
     it("Le mot-clef est copié", async () => {
+      const motsClefs = await obtenir(({ siPasVide }) =>
+        client.motsClefs.suivreMotsClefs({
+          f: siPasVide(),
+        }),
+      );
       expect(Array.isArray(motsClefs)).to.be.true();
       expect(motsClefs).to.contain(idMotClef2);
     });
 
     it("Les noms sont copiés", async () => {
-      const val = await noms.attendreExiste();
-      expect(val).to.deep.equal({ த: "நீரியல்", हिं: "जल विज्ञान" });
+      const noms = await obtenir<TraducsNom>(({ siDéfini }) =>
+        client.motsClefs.suivreNomsMotClef({
+          idMotClef: idMotClef2,
+          f: siDéfini(),
+        }),
+      );
+      expect(noms).to.deep.equal({ த: "நீரியல்", हिं: "जल विज्ञान" });
     });
 
     it("Les descriptions sont copiés", async () => {
-      const val = await descriptions.attendreExiste();
-      expect(val).to.deep.equal({
+      const descriptions = await obtenir<TraducsNom>(({ siDéfini }) =>
+        client.motsClefs.suivreDescriptionsMotClef({
+          idMotClef: idMotClef2,
+          f: siDéfini(),
+        }),
+      );
+      expect(descriptions).to.deep.equal({
         த: "நீரியலுக்காக ஒரு சிறப்பு சொலு",
         हिं: "जल विज्ञान के आँकड़ों के लिये",
       });
