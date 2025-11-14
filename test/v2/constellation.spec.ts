228c47b86840bb6ea3d58b7dba60ff25d5903955
julienmalard <julien.malard@mail.mcgill.ca>
2025-11-14 09:46:37 +0530

    Tests connectés au serveur relai local

--- test/v2/constellation.spec.ts
+++ test/v2/constellation.spec.ts
@@ -23,6 +23,14 @@ describe.only("Constellation", function () {
       expect(Object.values(constls[0].services).every((s) => s.estDémarré));
     });
 
+    it("instances de Constellation test non connectées au relai production", async () => {
+      const libp2p = await constls[0].services["libp2p"].libp2p();
+      const connexions = libp2p.getConnections();
+
+      expect(connexions.filter(c=>c.remoteAddr.getComponents().find(a=>a.name.includes('ip')))).to.not.be.empty();
+      expect(connexions.filter(c=>c.remoteAddr.getComponents().find(a=>a.name.includes('dns')))).to.be.empty();
+    })
+
     it("fermeture", async () => {
       await constls[0].fermer();
       expect(Object.values(constls[0].services).every((s) => !s.estDémarré));
