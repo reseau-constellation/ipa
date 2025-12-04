a81f0413d60d98dc5ca606ff5629d051f33e8ece
julienmalard <julien.malard@mail.mcgill.ca>
2025-12-04 16:16:26 +0530

    Importateur Omeka-S

---
+++ src/importateur/omeka-s.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import type { élémentDic } from "./json.js";
+
+export const importerDonnéesOmekaS = async ({
+  instance,
+}: {
+  instance: string;
+}): Promise<élémentDic[]> => {
+  const urlBase = `${instance}/api/items/`;
+  const réponseBase = await axios.get(urlBase);
+  const liens: string = réponseBase.headers["Link"];
+  // <https://ifp-s-intg.inist.fr/api/items?sort_by=id&sort_order=asc&page=1>; rel="first", <https://ifp-s-intg.inist.fr/api/items?sort_by=id&sort_order=asc&page=2>; rel="next", <https://ifp-s-intg.inist.fr/api/items?sort_by=id&sort_order=asc&page=1115>; rel="last"
+  const nPages = Number(
+    liens.slice(liens.lastIndexOf("&page=") + 6).replace('>; rel="last"', ""),
+  );
+  let données: élémentDic[] = [];
+  for (const i in Array(nPages).keys()) {
+    const urlPage = `${instance}/api/items?page=${i + 1}`;
+    données = [...données, ...(await axios.get(urlPage)).data];
+  }
+  return données;
+};
