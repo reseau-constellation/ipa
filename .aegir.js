8ac4509d9d584a0803b173fbefe23b274eb595af
julienmalard <julien.malard@mail.mcgill.ca>
2026-02-14 16:13:55 +0530

    Plus utilisé

--- .aegir.js
+++ .aegir.js
@@ -1,17 +1,8 @@
-import { copyFileSync, mkdtempSync } from "fs";
-import os from "os";
-import path, { dirname } from "path";
-import url, { fileURLToPath } from "url";
-import {
-  obtConfigEsbuild,
-  générerConfigÆgir,
-  dossierTempo,
-} from "@constl/utils-tests";
+import path from "path";
+import url from "url";
+import { générerConfigÆgir } from "@constl/utils-tests";
 import cors from "cors";
-import esbuildCmd from "esbuild";
-import { $ } from "execa";
 import express from "express";
-import { sync } from "rimraf";
 
 const générerServeurRessourcesTests = async (opts, idsPairs) => {
   /**
@@ -61,122 +52,7 @@ const générerServeurRessourcesTests = async (opts, idsPairs) => {
   return async () => serveurLocal?.close();
 };
 
-const lancerSfipDansNode = async (_opts) => {
-  const { dossier, fEffacer } = await dossierTempo();
-  const args = ["--dossier", dossier];
-
-  const processusNode = $`node dist/test/utils/lancerNœud.js ${args} &`; // $({stdio: 'inherit'})`...` pour écrire à la console
-  processusNode.catch((e) => {
-    if (e.signal !== "SIGTERM") throw new Error(e);
-  });
-  processusNode.stdout.on("data", (x) => {
-    const texte = new TextDecoder().decode(x);
-    console.log(texte);
-  });
-  const idPair = await new Promise((résoudre) => {
-    const fDonnées = (x) => {
-      const texte = new TextDecoder().decode(x);
-      if (texte.startsWith("SFIP initialisé avec id de nœud :")) {
-        processusNode.stdout.off("data", fDonnées);
-        résoudre(texte.split("\n")[0].split(":")[1].trim());
-      }
-    };
-    processusNode.stdout.on("data", fDonnées);
-  });
-  return {
-    idPair,
-    fermerNode: async () => {
-      processusNode?.kill();
-      fEffacer();
-    },
-  };
-};
-
-const lancerSfipDansNavigateur = async (_opts) => {
-  const { chromium } = await import("playwright");
-  const navigateur = await chromium.launch();
-
-  const dossierCompilation = mkdtempSync(
-    path.join(os.tmpdir(), "test-constl-"),
-  );
-  let idPair;
-  try {
-    const fichierJs = path.join(dossierCompilation, "test.min.js");
-    const page = await navigateur.newPage();
-    page.on("console", (msg) => {
-      if (
-        !msg
-          .text()
-          .includes(
-            "Failed to load resource: the server responded with a status of 404",
-          )
-      )
-        console.log("Message de Playwright : ", msg.text());
-    });
-
-    const globalName = "testnavigsfip";
-    const umdPre = `(function (root, factory) {(typeof module === 'object' && module.exports) ? module.exports = factory() : root.${globalName} = factory()}(typeof self !== 'undefined' ? self : this, function () {`;
-    const umdPost = `return ${globalName}}));`;
-    const configEsbuild = await obtConfigEsbuild();
-    await esbuildCmd.build({
-      entryPoints: ["dist/test/utils/lancerNœud.js"],
-      bundle: true,
-      format: "iife",
-      conditions: ["production"],
-      outfile: fichierJs,
-      banner: { js: umdPre },
-      footer: { js: umdPost },
-      define: {
-        global: "globalThis",
-        "process.env.NODE_ENV": '"production"',
-      },
-      ...configEsbuild,
-    });
-    const fichierHtml = path.join(dossierCompilation, "lancerNœud.html");
-
-    const __dirname = dirname(fileURLToPath(import.meta.url));
-    copyFileSync(
-      path.join(__dirname, "test", "utils", "lancerNœud.html"),
-      fichierHtml,
-    );
-
-    const promesseIdPair = new Promise((résoudre) => {
-      const fDonnées = (x) => {
-        const texte = x.text();
-        if (texte.startsWith("SFIP initialisé avec id de nœud :")) {
-          page.off("console", fDonnées);
-          const id = texte.split("\n")[0].split(":")[1].trim();
-          résoudre(id);
-        }
-      };
-      page.on("console", fDonnées);
-    });
-    await page.goto(`file://${fichierHtml}`);
-    idPair = await promesseIdPair;
-  } catch (e) {
-    // On arrête pas les tests pour une petite erreur comme ça
-    console.error(e);
-  }
-
-  return {
-    idPair,
-    fermerNavigateur: async () => {
-      await navigateur.close();
-      sync(dossierCompilation);
-    },
-  };
-};
-
 const avantTest = async (opts) => {
-  // On va lancer une page Constellation pour pouvoir tester la connectivité webrtc avec les navigateurs
-  // const { fermerNavigateur, idPair: idPairNavig } =
-  //   await lancerSfipDansNavigateur(opts);
-  // console.log({ idPairNavig });
-
-  // Et une sur Node.js pour pouvoir tester la connectivité avec Node
-  // const { fermerNode, idPair: idPairNode } = await lancerSfipDansNode(opts);
-  // console.log({ idPairNode });
-
   // Pour pouvoir accéder les fichiers test dans le navigateur
   const fermerServeurLocal = await générerServeurRessourcesTests(opts, {
     // idPairNode,
