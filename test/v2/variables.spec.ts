51739c458852c6efb59f6b8d0aea7ccefd08eda2
julienmalard <julien.malard@mail.mcgill.ca>
2026-03-27 10:42:18 +0530

    Activer tests variables

--- test/v2/variables.spec.ts
+++ test/v2/variables.spec.ts
@@ -22,7 +22,7 @@ import type {
   RègleVariableAvecId,
 } from "@/v2/règles.js";
 
-describe.skip("Variables", function () {
+describe.only("Variables", function () {
   let fermer: () => Promise<void>;
   let constls: Constellation[];
   let constl: Constellation;
