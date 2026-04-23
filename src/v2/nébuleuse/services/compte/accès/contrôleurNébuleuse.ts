423c8b99ddda3fb688f4532f6347d04591beda04
julienmalard <julien.malard@mail.mcgill.ca>
2026-04-23 12:48:58 +0530

    Utiliser estErreurAvortée

--- src/v2/nébuleuse/services/compte/accès/contrôleurNébuleuse.ts
+++ src/v2/nébuleuse/services/compte/accès/contrôleurNébuleuse.ts
@@ -31,6 +31,7 @@ import { ComposedStorage, IPFSBlockStorage, LRUStorage } from "@orbitdb/core";
 
 import { v4 as uuidv4 } from "uuid";
 import PQueue from "p-queue";
+import { estErreurAvortée } from "@/v2/nébuleuse/utils.js";
 import { mandatOrbite } from "../../orbite/mandat.js";
 import { appelerLorsque } from "../../utils.js";
 import { MODÉRATRICE, rôles } from "./consts.js";
@@ -210,7 +211,7 @@ const ContrôleurNébuleuse =
       try {
         identitéÉcrivain = await identities.getIdentity(entry.identity, signal);
       } catch (e) {
-        if (e.toString().includes("AbortError")) return false;
+        if (estErreurAvortée(e)) return false;
         throw e;
       }
       if (!identitéÉcrivain) {
