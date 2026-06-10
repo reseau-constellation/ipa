49ccf18b9fe1bcc8ada1414aca9cd59b0ce76726
julienmalard <julien.malard@mail.mcgill.ca>
2026-06-10 11:40:26 +0530

    Test additionnel

--- test/v2/nébuleuse/services/compte/accès.spec.ts
+++ test/v2/nébuleuse/services/compte/accès.spec.ts
@@ -315,6 +315,41 @@ describe("Accès", function () {
         expect(estAutorisé).to.be.true();
       }
     });
+
+    it("invitations trasitives par d'autres modératrices après fermeture de la bd", async () => {
+      // Autoriser orbite 2 comme modératrice
+      let accès = bd.access as InstanceContrôleurNébuleuse
+      accès.autoriser(MODÉRATRICE, orbite2.identity.id)
+
+      const bdSurOrbite2 = await orbite2.open(bd.address);
+      const accèsSurOrbite2 = bdSurOrbite2.access as InstanceContrôleurNébuleuse;
+      await attendreQue(() =>
+        accèsSurOrbite2.estUneModératrice(orbite2.identity.id),
+      );
+
+      // Fermer et rouvrir la bd sur orbite 1
+      await bd.close();
+      bd = (await orbite1.open(bd.address, {
+        type: "keyvalue",
+      })) as KeyValueDatabase;
+      accès = bd.access as InstanceContrôleurNébuleuse;
+      
+      // Orbite 2 ajoute orbite 3 comme modératrice
+      await (accèsSurOrbite2).autoriser(
+        MODÉRATRICE,
+        orbite3.identity.id,
+      );
+
+      // Orbite 1 accepte l'ajout
+      await obtenir<AccèsDispositif[]>(({ si }) =>
+        (accès).suivreDispositifsAutorisées(
+          si((x) => !!x.find((d) => d.idDispositif === orbite3.identity.id)),
+        ),
+      );
+      const estAutorisé = await accès.estAutorisé(orbite3.identity.id);
+      expect(estAutorisé).to.be.true();
+
+    });
   });
 
   describe("par identité utilisateur", function () {
