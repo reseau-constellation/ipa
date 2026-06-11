834fad36c317b778ba987ab729b1e989ad572207
julienmalard <julien.malard@mail.mcgill.ca>
2026-06-11 10:28:06 +0530

    Pour Windows + déboguage

--- test/v2/automatisations.spec.ts
+++ test/v2/automatisations.spec.ts
@@ -38,6 +38,7 @@ import { enleverPréfixesEtOrbite } from "@/v2/utils.js";
 import { ImportateurFeuilleCalcul } from "@/v2/importateur/feuille.js";
 import {
   créerConstellationsTest,
+  journalifier,
   obtenir,
   utiliserFauxChronomètres,
 } from "./utils.js";
@@ -592,7 +593,7 @@ describe("Automatisations", function () {
             },
             {
               nom: "mon fichier2",
-              chemin: "./fichier2.png",
+              chemin: join(".", "fichier2.png"),
               données: new TextEncoder().encode("efgh"),
               idc: "bafybeicktzgg5fjm2v5wsqzvo6sqau35ffq5gerllu3lxalfdxjjmv63em",
             },
@@ -624,6 +625,7 @@ describe("Automatisations", function () {
           écrireDonnées(donnéesFichier, adresseFichier);
 
           // Tester l'automatisation
+          console.log("ici 1")
           idAuto = await constl.automatisations.ajouterAutomatisationImporter({
             idBd,
             idTableau,
@@ -641,17 +643,17 @@ describe("Automatisations", function () {
               { colonne: colFichier, conversion: { type: "fichier" } },
             ],
           });
-
+          console.log("ici 2")
           const donnéesTableau = await obtenir<
             DonnéesRangéeTableauAvecId<DonnéesRangéeTableau>[]
           >(({ si }) =>
             constl.bds.tableaux.suivreDonnées({
               idStructure: idBd,
               idTableau,
-              f: stabiliser()(si((x) => !!x && x.length >= 3)),
+              f: journalifier( stabiliser()(si((x) => !!x && x.length >= 3)), "suivre données"),
             }),
           );
-
+          console.log("ici 2")
           expect(donnéesTableau.map((d) => d.données)).to.have.deep.members(
             réfDonnées,
           );
@@ -662,7 +664,7 @@ describe("Automatisations", function () {
             const idSfip = donnéesTableau.find(
               (d) => d.données[colNom] === nom,
             )!.données[colFichier] as string;
-
+            console.log("ici 3", nom)
             expect(
               await hélia.obtFichierDeSFIP({
                 id: idSfip,
