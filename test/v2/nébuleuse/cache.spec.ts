1ac052e66da7c55e99ba7a33f97a6e61bd82dced
julienmalard <julien.malard@mail.mcgill.ca>
2026-08-28 11:30:23 +0530

    Instantiation

--- test/v2/nébuleuse/cache.spec.ts
+++ test/v2/nébuleuse/cache.spec.ts
@@ -191,7 +191,7 @@ describe("Cache", function () {
       fibonnaci(n: number): number[] {
         let a = 0,
           b = 1,
-          c = 0;
+          c: number;
         const fib = [a, b];
         for (let i = 2; i < n; i++) {
           c = a + b;
